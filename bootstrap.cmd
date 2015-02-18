4f5baa29b4fd94d0fce36a1eacce0d2814570654
seapagan <seapagan@gmail.com>
2015-02-18 23:23:46 +0000

    Bootstrap : unpack TinyPerl with basic error checks
    Now ready to fork TinyPerl and run the main download / install bootstrap

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -12,7 +12,7 @@ rem note that there is limited error checking or idiot-proofing in the file at t
 
 SETLOCAL
 echo dev-shell bootstrap v0.1 (c) seapagan 2015
-echo.  
+echo.
 echo Downloading the 'TinyPerl' distribution ...
 %~dp0mingw32\wget -q --show-progress -c --trust-server-names -c --directory-prefix=%~dp0support\packages http://sourceforge.net/projects/tinyperl/files/tinyperl/2.0/tinyperl-2.0-580-win32.zip/download
 if NOT EXIST %~dp0support\packages\tinyperl-2.0-580-win32.zip (
@@ -23,8 +23,16 @@ if NOT EXIST %~dp0support\packages\tinyperl-2.0-580-win32.zip (
 )
 echo.
 echo Unpacking TinyPerl ...
-%~dp0support\unzip.exe %~dp0support\packages\tinyperl-2.0-580-win32.zip -d %~dp0support
-move %~dp0support\tinyperl* %~dp0support\tinyperl
+rem erase TinyPerl directory first if exists ...
+if EXIST "%~dp0support\tinyperl\" (
+RD /S /Q %~dp0support\tinyperl
+)
+%~dp0support\unzip.exe -q %~dp0support\packages\tinyperl-2.0-580-win32.zip -d %~dp0support
+move %~dp0support\tinyperl* %~dp0support\tinyperl > NUL
+if NOT EXIST "%~dp0support\tinyperl\" (
+	echo "Failure to unpack the TinyPerl distribution, please report any error messages, thanks."
+	pause
+	exit /b 254
+)
 ENDLOCAL
 pause
-
