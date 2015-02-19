fa2605039fe1599b965ccccd6cf5b91ac481f9b4
seapagan <seapagan@gmail.com>
2015-02-19 18:16:21 +0000

    Rebase download code
    Moved code to read urls into a sub, and auto create the relevant cache
    subdirs if needed instead of keeping in repo with a dummy file.

--- support/bootstrap/bootstrap.pl
+++ support/bootstrap/bootstrap.pl
@@ -2,7 +2,7 @@ use strict;
 use File::Basename;
 use Cwd 'abs_path';
 
-# NOTE : We need to get the base path of the system, but in TinyPel $0 does not work so it is difficult
+# NOTE : We need to get the base path of the system, but in TinyPerl $0 does not work so it is difficult
 # to get the proper script location as the usual standard methods will fail.
 # To get past this when we run this from the standard bootstrap.cmd, first we CD to the bootstrap directory.
 # This allows us to run the script from its own directory directly for testing.
@@ -26,15 +26,18 @@ print "\nStage 2 : Download and unpack assorted support utilities.\n\n";
 print "\nStage 3 : Download and MSYS packages to local cache.\n\n";
 # load the MSYS URL's into an array from the file 'msys-urls'...
 my $path_to_urls = $package_directory."/msys-urls";
-open my $handle, '<', $path_to_urls;
-chomp(my @urls = <$handle>);
-close $handle;
-# Remove empty lines and headings...
-my @msysurls = grep(/S/, @urls);
+my @msysurls = geturls($path_to_urls);
+
 
 # Download these URL's ..
 my $msys_cache = $package_directory."/msys";
 my $url;
+
+# create the MSYS Cache directory if it does not exist...
+if (!-d $msys_cache) {
+  mkdir $msys_cache or die "Cannot create Cache directory for MSYS!";
+}
+
 foreach $url (@msysurls) {
   #get the actual filename, from the last part of the URL, removing the SorceForge '/download' text...
   my $filename = basename(substr($url, 0, -9));
@@ -64,4 +67,19 @@ foreach $url (@msysurls) {
       #print "$filename is an XZ file! ($ext)\n";
     }
   }
+}
+
+
+
+
+# user functions
+sub geturls() {
+  # when provided with a file containing URLS, will return an array containing them.
+  # 1 parameter - $path_to_urls
+  ($path_to_urls) = @_;
+  open my $handle, '<', $path_to_urls;
+  chomp(my @urls = <$handle>);
+  close $handle;
+  # Remove empty lines and headings then return ...
+  return grep(/S/, @urls);
 }
