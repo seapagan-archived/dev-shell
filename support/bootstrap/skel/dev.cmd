2914e67281c8763771485919ce3edcdd66671abc
seapagan <seapagan@gmail.com>
2015-03-12 17:38:23 +0000

    Update dev.cmd
    Changed some old paths from a previous setup to the new naming
    convention

--- support/bootstrap/skel/dev.cmd
+++ support/bootstrap/skel/dev.cmd
@@ -10,15 +10,15 @@ set LOCALAPPDATA=%HOMEPATH%\AppData\Local
 set USERNAME=seapagan
 set COMPUTERNAME=development
 set HOSTNAME=%COMPUTERNAME%
-set TMP=%~dp0root\tmp
+set TMP=%~dp0msys\tmp
 
 rem set location of SSL certificates...
 set SSL_CERT_FILE=%HOME%\ca-bundle.crt
 
 rem update the fstab to point to the correct drive, as it may have changed...
-echo %~dp0mingw32 /mingw > %~dp0root\etc\fstab
-echo %~dp0local /usr/local >> %~dp0root\etc\fstab
-echo %~dp0home /home >> %~dp0root\etc\fstab
+echo %~dp0mingw32 /mingw > %~dp0msys\etc\fstab
+echo %~dp0local /usr/local >> %~dp0msys\etc\fstab
+echo %~dp0home /home >> %~dp0msys\etc\fstab
 
 cd %HOME%
 
