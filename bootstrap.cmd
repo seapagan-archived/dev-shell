1df604d1f0e8b012e8ca248773ee1b65ba45acf2
seapagan <seapagan@gmail.com>
2015-03-13 20:08:17 +0000

    move md5deep download to original batch file
    We really need to have this (md5deep.exe) downloaded before the Perl
    script is called, so that all the archive downloads can be validated, so
    move download and unpack of this to the windows cmd file

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -10,15 +10,22 @@ rem ****************************************************************************
 
 rem note that there is limited error checking or idiot-proofing in the file at this time.
 
-
 echo dev-shell bootstrap v0.1 (c) seapagan 2015
 echo.
-echo Stage 1 : Download and unpack the 'TinyPerl' distribution ...
+echo Stage 1 : Download and unpack some required packages ...
+rem tinyperl ...
 if NOT EXIST %~dp0support\packages\tinyperl-2.0-580-win32.zip (
-  %~dp0support\bootstrap\wget -q --show-progress -c --trust-server-names -c --directory-prefix=%~dp0support\packages http://sourceforge.net/projects/tinyperl/files/tinyperl/2.0/tinyperl-2.0-580-win32.zip/download
+  %~dp0support\bootstrap\wget -q --show-progress -c --trust-server-names --directory-prefix=%~dp0support\packages http://sourceforge.net/projects/tinyperl/files/tinyperl/2.0/tinyperl-2.0-580-win32.zip/download
 ) ELSE (
 	ECHO TinyPerl package already exists, skipping download.
 )
+rem md5deep ...
+if NOT EXIST %~dp0support\packages\md5deep-4.4.zip (
+  %~dp0support\bootstrap\wget -q --show-progress -c --directory-prefix=%~dp0support\packages https://github.com/jessek/hashdeep/releases/download/release-4.4/md5deep-4.4.zip
+) ELSE (
+	ECHO MD5Deep package already exists, skipping download.
+)
+
 echo.
 rem erase TinyPerl directory first if exists ...
 if EXIST "%~dp0support\tinyperl\" (
@@ -30,6 +37,14 @@ if NOT EXIST "%~dp0support\tinyperl\" (
 	echo Failure to unpack the TinyPerl distribution, please report any error messages, thanks.
 	pause
 	exit /b 254
+)
+
+rem unpack the md5deep.exe ...
+%~dp0support\bootstrap\unzip.exe -j -q -o %~dp0support\packages\md5deep-4.4.zip "md5deep-4.4\md5deep.exe" -d %~dp0support
+if NOT EXIST "%~dp0support\md5deep.exe" (
+	echo Failure to unpack the MD5Deep distribution, please report any error messages, thanks.
+	pause
+	exit /b 253
 )
 
 rem now run the perl script to continue...
