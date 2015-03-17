b304b3c382252014024ed007c57fe89d46bafed8
seapagan <seapagan@gmail.com>
2015-03-17 14:46:28 +0000

    Simplify 'stage' management
    added a counter for each stage of the process, easier to insert others
    in the future.

--- support/bootstrap/bootstrap.pl
+++ support/bootstrap/bootstrap.pl
@@ -22,9 +22,12 @@ my $path_to_urls;
 create_dir($dirs{"root"}."/msys");
 create_dir($dirs{"root"}."/mingw32");
 
+# counter variable for the stages, make adding new ones easier ;)
+my $stage_counter = 2;
+
 # ------------------------------------------------------------------------------
 # We need a few support utilities, this will also include Console and ANSICON.
-print "Stage 2 : Download assorted support utilities.\n\n";
+print "Stage $stage_counter : Download assorted support utilities.\n\n";
 # load the Support tools URL's into an array from the file 'msys-urls'...
 $path_to_urls = $dirs{"base"}."/urls/support-urls";
 my ($toolsurls, $toolsfiles) = geturls($path_to_urls);
@@ -36,9 +39,11 @@ $path_to_urls = $dirs{"base"}."/urls/extras-urls";
 my ($extraurls, $extrafiles) = geturls($path_to_urls);
 my @extra_filenames = getfiles($dirs{"package"}, @$extraurls);
 my @extra_filespecs = @$extrafiles;
+$stage_counter++;
+
 
 # ------------------------------------------------------------------------------
-print "\nStage 3 : Download MSYS packages to local cache.\n\n";
+print "\nStage $stage_counter : Download MSYS packages to local cache.\n\n";
 # load the MSYS URL's into an array from the file 'msys-urls'...
 $path_to_urls = $dirs{"base"}."/urls/msys-urls";
 my ($msysurls, $msysfiles) = geturls($path_to_urls);
@@ -48,10 +53,11 @@ create_dir($msys_cache);
 # get all the MSYS packages we need...
 my @msys_filenames = getfiles($msys_cache, @$msysurls);
 my @msys_filespecs = @$msysfiles;
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 4 : Download MinGW packages to local cache.\n\n";
+print "\nStage $stage_counter : Download MinGW packages to local cache.\n\n";
 # load the minGW URL's into an array from the file 'mingw-urls'...
 $path_to_urls = $dirs{"base"}."/urls/mingw-urls";
 my ($mingwurls, $mingwfiles) = geturls($path_to_urls);
@@ -61,10 +67,11 @@ create_dir($mingw_cache);
 # get all the minGW packages we need...
 my @mingw_filenames = getfiles($mingw_cache, @$mingwurls);
 my @mingw_filespecs = @$mingwfiles;
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 5 : Download TDM GCC Compiler packages to local cache.\n\n";
+print "\nStage $stage_counter : Download TDM GCC Compiler packages to local cache.\n\n";
 # load the GCC URL's into an array from the file 'tdm-gcc-urls'...
 $path_to_urls = $dirs{"base"}."/urls/tdm-gcc-urls";
 my ($gccurls, $gccfiles) = geturls($path_to_urls);
@@ -74,10 +81,11 @@ create_dir($tdm_cache);
 # get all the GCC packages we need...
 my @gcc_filenames = getfiles($tdm_cache, @$gccurls);
 my @gcc_filespecs = @$gccfiles;
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 6 : Download Updated packages to local cache.\n\n";
+print "\nStage $stage_counter : Download Updated packages to local cache.\n\n";
 # load the URL's into an array from the file 'local-urls'...
 $path_to_urls = $dirs{"base"}."/urls/local-urls";
 my ($localurls, $localfiles) = geturls($path_to_urls);
@@ -87,10 +95,11 @@ create_dir($local_cache);
 # get all the packages we need...
 my @local_filenames = getfiles($local_cache, @$localurls);
 my @local_filespecs = @$localfiles;
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 7 : Unpack support utilities.\n";
+print "\nStage $stage_counter : Unpack support utilities.\n";
 # Unpack 7za, console, ANSICON etc.
 # ------------------------------------------------
 # : Source path is $dirs{"package"}
@@ -99,10 +108,11 @@ print "\nStage 7 : Unpack support utilities.\n";
 # : FileSpecs (those to be unpacked) are stored in @util_filespecs.
 # ------------------------------------------------
 $result = unpack_file($dirs{"package"}, $dirs{"support"}, \@util_filenames, \@util_filespecs);
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 8 : Unpack Extra packages into MinGW.\n";
+print "\nStage $stage_counter : Unpack Extra packages into MinGW.\n";
 # Unpack cmake, dmake and others than need to be in MinGW.
 # ------------------------------------------------
 # : Source path is $dirs{"package"}
@@ -111,10 +121,11 @@ print "\nStage 8 : Unpack Extra packages into MinGW.\n";
 # : FileSpecs (those to be unpacked) are stored in @extra_filespecs.
 # ------------------------------------------------
 $result = unpack_file($dirs{"package"}, $dirs{"mingw"}, \@extra_filenames, \@extra_filespecs);
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 9 : Unpack MSYS.\n";
+print "\nStage $stage_counter : Unpack MSYS.\n";
 # Unpack MSYS distribution.
 # ------------------------------------------------
 # : Source path is $msys_cache
@@ -123,10 +134,11 @@ print "\nStage 9 : Unpack MSYS.\n";
 # : FileSpecs (those to be unpacked) are stored in @msys_filespecs.
 # ------------------------------------------------
 $result = unpack_file($msys_cache, $dirs{"msys"}, \@msys_filenames, \@msys_filespecs);
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 10 : Unpack MinGW.\n";
+print "\nStage $stage_counter : Unpack MinGW.\n";
 # Unpack MinGW distribution.
 # ------------------------------------------------
 # : Source path is $mingw_cache
@@ -135,10 +147,11 @@ print "\nStage 10 : Unpack MinGW.\n";
 # : FileSpecs (those to be unpacked) are stored in @mingw_filespecs.
 # ------------------------------------------------
 $result = unpack_file($mingw_cache, $dirs{"mingw"}, \@mingw_filenames, \@mingw_filespecs);
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 11 : Unpack GCC Packages.\n";
+print "\nStage $stage_counter : Unpack GCC Packages.\n";
 # Unpack GCC distribution.
 # ------------------------------------------------
 # : Source path is $tdm_cache
@@ -147,10 +160,11 @@ print "\nStage 11 : Unpack GCC Packages.\n";
 # : FileSpecs (those to be unpacked) are stored in @gcc_filespecs.
 # ------------------------------------------------
 $result = unpack_file($tdm_cache, $dirs{"mingw"}, \@gcc_filenames, \@gcc_filespecs);
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 12 : Unpack Updated Packages.\n";
+print "\nStage $stage_counter : Unpack Updated Packages.\n";
 # Unpack GCC distribution.
 # ------------------------------------------------
 # : Source path is $local_cache
@@ -159,10 +173,11 @@ print "\nStage 12 : Unpack Updated Packages.\n";
 # : FileSpecs (those to be unpacked) are stored in @local_filespecs.
 # ------------------------------------------------
 $result = unpack_file($local_cache, $dirs{"mingw"}, \@local_filenames, \@local_filespecs);
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "\nStage 13 : Tidy up base system, removing unneeded files.\n";
+print "\nStage $stage_counter : Tidy up base system, removing unneeded files.\n";
 # There are a few files in the standard MSYS distro that are not needed in this particular system...
 # note that as of now there is no error checking ...
 
@@ -175,10 +190,11 @@ foreach my $unwanted (@unwanted_msys) {
 # also remove the postinstall directory as we will do this manually ourselves...
 rmtree($dirs{"msys"}."/postinstall");
 print " -- Done\n";
+$stage_counter++;
 
 
 # ------------------------------------------------------------------------------
-print "Stage 14 : Finalize Environment - Copy final files.\n";
+print "Stage $stage_counter : Finalize Environment - Copy final files.\n";
 # Give us a working system by copying the needed skeleton files and startup batch ...
 
 # # create the home and local directories if not already there and the tmp ..
@@ -198,3 +214,4 @@ dircopy($dirs{"base"}."/skel/support/.", $dirs{"support"}) or die "Failed to cop
 dircopy($dirs{"base"}."/skel/home/.", $dirs{"root"}."/home") or die "Failed to copy skeleton files: $!";
 dircopy($dirs{"base"}."/skel/etc/.", $dirs{"msys"}."/etc") or die "Failed to copy skeleton files: $!";
 print " -- Done\n";
+$stage_counter++;
