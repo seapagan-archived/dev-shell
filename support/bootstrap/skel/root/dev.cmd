ee6850c1b020c3ede62791bc0f9a07f98cc2e7e3
seapagan <seapagan@gmail.com>
2015-03-17 11:06:10 +0000

    fixed last commit
    Bad juju in the .gitignore caused dev.cmd skel to be ignored.

---
+++ support/bootstrap/skel/root/dev.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+set HOMEPATH=%~dp0home
+set HOME=%HOMEPATH%
+set USERPROFILE=%HOMEPATH%
+
+set APPDATA=%HOMEPATH%\AppData\Roaming
+set LOCALAPPDATA=%HOMEPATH%\AppData\Local
+
+set USERNAME=seapagan
+set COMPUTERNAME=development
+set HOSTNAME=%COMPUTERNAME%
+set TMP=%~dp0msys\tmp
+
+rem set location of SSL certificates...
+set SSL_CERT_FILE=%HOME%\ca-bundle.crt
+
+rem update the fstab to point to the correct drive, as it may have changed...
+echo %~dp0mingw32 /mingw > %~dp0msys\etc\fstab
+echo %~dp0local /usr/local >> %~dp0msys\etc\fstab
+echo %~dp0home /home >> %~dp0msys\etc\fstab
+
+cd %HOME%
+
+start %~dp0support\console 
+
