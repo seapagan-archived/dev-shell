4613a53e2a86aa6495bb428adfa39775124002f2
seapagan <seapagan@gmail.com>
2015-03-19 10:00:50 +0000

    ConfigFile.pm  : Stop Perl issuing a warning
    Adding comments to the config.ini caused Perl to issue warnings about
    uninitialized variables in the regex (since $tokens[1] was not valid as
    line was not proper config setting pair). Fixed by moving the code to
    check for comments BEFORE the regex which makes more sense.

--- support/bootstrap/lib/My/ConfigFile.pm
+++ support/bootstrap/lib/My/ConfigFile.pm
@@ -18,10 +18,10 @@ sub read_config {
 
   foreach my $line (@config_lines) {
     my @tokens = split('=', $line);
-    # remove leading and trailing spaces...
-    $tokens[0] =~ s/^\s+|\s+$//g;
-    $tokens[1] =~ s/^\s+|\s+$//g;
-    if (not $tokens[0] =~ /^\s*\#/) { # ignore comments
+    if (!$tokens[0] =~ /^\s*\#/) { # ignore any comments
+      # remove leading and trailing spaces...
+      $tokens[0] =~ s/^\s+|\s+$//g;
+      $tokens[1] =~ s/^\s+|\s+$//g;
       $hash{$tokens[0]} = $tokens[1];
     }
   }
